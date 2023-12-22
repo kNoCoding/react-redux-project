66b92bda5bd7bf13787b53bf691594305ce2e902
Konstantin Nochevny <kosnobiz@gmail.com>
2023-12-22 18:02:21 +0200

    Implement todo.service.js

--- services/todo.service.js
+++ services/todo.service.js
@@ -1,2 +1,47 @@
 // todo.service.js
 
+import { storageService } from './async-storage.service.js'
+import { userService } from './user.service.js'
+
+const STORAGE_KEY = 'todoDB'
+
+export const todoService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyTodo
+}
+
+
+function query() {
+    // return axios.get(BASE_URL).then(res => res.data)
+    return storageService.query(STORAGE_KEY)
+}
+function getById(todoId) {
+    return storageService.get(STORAGE_KEY, todoId)
+}
+function remove(todoId) {
+    // return Promise.reject('Not now!')
+    return storageService.remove(STORAGE_KEY, todoId)
+}
+function save(todo) {
+    if (todo._id) {
+        return storageService.put(STORAGE_KEY, todo)
+    } else {
+        // when switching to backend - remove the next line
+        todo.owner = userService.getLoggedinUser()
+        return storageService.post(STORAGE_KEY, todo)
+    }
+}
+
+function getEmptyTodo() {
+    return {
+        txt: 'New todo' + (Date.now() % 1000),
+        isDone: false,
+    }
+}
+
+// TEST DATA
+// storageService.post(STORAGE_KEY, {vendor: 'Subali Rahok 6', price: 980}).then(x => console.log(x))
+
